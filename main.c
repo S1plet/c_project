dcaf8acccd3b298b288f64e4ed6bd9cf81c300d2
Félix Lang <123630435+S1plet@users.noreply.github.com>
2024-05-20 17:08:48 +0200

    Update main.c

--- main.c
+++ main.c
@@ -4,14 +4,18 @@
 #include "column.h"
 
 int main(){
+    // Example of the use of a dataframe
+    
     DATAFRAME* df = create_dataframe(3);
 
     fill_dataframe(df);
 
+    // Choose the name you want instead of "col_3"
     rename_column(df, 2, "col_3");
 
     display_dataframe(df);
 
+    // You can change the value from 4 to whatever you want
     display_number_of_cells_equal(df, 4);
 
     return 0;
